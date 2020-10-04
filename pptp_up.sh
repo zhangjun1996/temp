67e0671bbbacce16212cd73101c621acb723257c
zhangjun <2281979437@qq.com>
2020-10-04 23:24:45 +0800

    只允许第一个pptp客户端访问内网和校园网

--- pptp_up.sh
+++ pptp_up.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 # if there is no rule, add rules to nat table on POSTROUTING chan that nat source ip adrr 192.168.16.0/24 to internet
 iptables -C POSTROUTING -t nat -s 192.168.16.0/24 -j MASQUERADE || iptables -A POSTROUTING -t nat -s 192.168.16.0/24 -j MASQUERADE
-#disable data packge from ppp+ interface to 192.168.31.0/24(my router lan)
-iptables -t filter -A FORWARD -i ppp+ -d 192.168.31.0/24 -j REJECT
+#allow data packge from ppp0(my client) interface to 192.168.31.0/24(my router lan)
+iptables -t filter -C FORWARD -i ppp0 -d 192.168.31.0/24 -j ACCEPT || iptables -t filter -A FORWARD -i ppp0 -d 192.168.31.0/24 -j ACCEPT
+#disable data packge from ppp+i(others client) interface to 192.168.31.0/24(my router lan)
+iptables -t filter -C FORWARD -i ppp+ -d 192.168.31.0/24 -j REJECT || iptables -t filter -A FORWARD -i ppp+ -d 192.168.31.0/24 -j REJECT
 #disable data packge from ppp+ interface to 192.168.16.0/24(ppp+ device ip lan)
-iptables -t filter -A INPUT -i ppp+ -j DROP
+iptables -t filter -C INPUT -i ppp+ -j DROP || iptables -t filter -A INPUT -i ppp+ -j REJECT
