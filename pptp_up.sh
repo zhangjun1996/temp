4bace122cd50bb7ae765718987d76413552b6de4
zhangjun <2281979437@qq.com>
2020-10-04 23:11:15 +0800

    添加内网过滤规则

--- pptp_up.sh
+++ pptp_up.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 # if there is no rule, add rules to nat table on POSTROUTING chan that nat source ip adrr 192.168.16.0/24 to internet
 iptables -C POSTROUTING -t nat -s 192.168.16.0/24 -j MASQUERADE || iptables -A POSTROUTING -t nat -s 192.168.16.0/24 -j MASQUERADE
+#disable data packge from ppp+ interface to 192.168.31.0/24(my router lan)
+iptables -t filter -A FORWARD -i ppp+ -d 192.168.31.0/24 -j REJECT
+#disable data packge from ppp+ interface to 192.168.16.0/24(ppp+ device ip lan)
+iptables -t filter -A INPUT -i ppp+ -j DROP
